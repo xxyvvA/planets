33119a5f0daac4f3ad016241a8fcc35a13a2871c
Avvy <saberwatch@gmail.com>
2022-01-16 15:54:09 -0800

    starting

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      white: "#FFFFFF",
+      black: "#070724",
+      darkgray: "#38384F",
+      lightgray: "#838391",
+      pastelblue: "#419EBB",
+      yellow: "#EDA249",
+      purple: "#6f2ed6",
+      lighterred: "#D14C32",
+      darkerred: "#D83A34",
+      orange: "#CD5120",
+      turquoise: "#1ec2a4",
+      blue: "#2d68f0",
+    },
+    fontFamily: {
+      heading: "Antonio",
+      body: "Spartan",
+    },
+  },
+  plugins: [],
+};
